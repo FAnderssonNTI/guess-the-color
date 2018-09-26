690dffbd198e0006ef6b5779b5c2971103ab1777
FAnderssonNTI <filip.andersson@elev.ga.ntig.se>
2018-09-26 13:40:26 +0200

    started making the actual code

--- guess the color.py
+++ guess the color.py
@@ -1,7 +1,5 @@
 import random
 
-color = random.randint(1, 4)
-
 clubs = """\
  _________ 
 |A        |
@@ -12,6 +10,15 @@ clubs = """\
 |   ~~~  +|
 |        V|
  ~~~~~~~~~
+"""
+clubsSymbol = """\
+     .-~~-.
+    {      }
+ .-~-.    .-~-.
+{              }
+ `.__.'||`.__.'
+       ||
+      '--`
 """
 diamonds = """\
  _________ 
@@ -24,6 +31,15 @@ diamonds = """\
 |        V|
  ~~~~~~~~~
 """
+diamondsSymbol = """\
+     /\
+   .'  `.
+  '      `.
+<          >
+ `.      .'
+   `.  .'
+     \/
+"""
 hearts = """\
  _________ 
 |A        |
@@ -34,6 +50,16 @@ hearts = """\
 |   `.'  #|
 |        V|
  ~~~~~~~~~
+"""
+heartsSymbol = """\
+       /\
+     .'  `.
+    '      `.
+ .'          `.
+{              }
+ ~-...-||-...-~
+       ||
+      '--`
 """
 spades = """\
  _________ 
@@ -46,10 +72,28 @@ spades = """\
 |        V|
  ~~~~~~~~~
 """
+spadesSymbol = """\
+ .-~~~-__-~~~-.
+{              }
+ `.          .'
+   `.      .'
+     `.  .'
+       \/
+"""
 
 
+menu = 0
+balance = 100
 
-print(clubs)
-print(diamonds)
-print(hearts)
-print(spades)
+
+while menu !=  2:
+    print("YOU HAVE ", balance, "SEK IN YOUR ACCOUNT RIGHT NOW\n" + "PRESS 1 TO TRY TO GUESS THE CARD\n" + "PRESS 2 TO TAKE YOUR EARNINGS AND GO\n")
+    try:
+        menu = int(input("MAKE YOUR CHOICE\n"))
+    except:
+        print("NOT A NUMBER")
+    if menu == 1:
+       color = random.randint(1, 4)
+       print("THE MACHINE HAS RANDOMED A COLOR\n" + "PRESS 1 IF YOU THINK THE COLOR IS CLUBS\n" + "PRESS 2 IF YOU THINK THE COLOR IS DIAMONDS\n" + "PRESS 3 IF YOU THINK THE COLOR IS HEARTS\n" + "PRESS 4 IF YOU THINK THE COLOR IS SPADES\n")
+    elif menu == 2:
+        print("YOUR MONEY WILL NOW BE PAYED OUT TO YOUR ACCOUNT\n" + "GOODBYE")
